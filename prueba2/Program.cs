cfa993632f0d71744704540d09f5475f5ddb3d34
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-05 18:49:16 -0500

    Agregar archivos de proyecto.

---
+++ prueba2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Que categoria le gustaria");
+        Console.WriteLine("1. De 3 estrellas");
+        Console.WriteLine("2. De 5 estrellas");
+        Console.Write("Seleccione una opción (1/2): ");
+
+
+        string categoriadetexto = Console.ReadLine();
+        int opcion = int.Parse(categoriadetexto);
+
+        Console.WriteLine("Dias de estadia :");
+
+        string diasdeestadiatxt = Console.ReadLine();
+        int DiasDeEstadia = int.Parse(diasdeestadiatxt);
+
+
+        float costodedesayuno = 7f;
+        float costopordia = 0f;
+
+        switch (categoriadetexto)
+        {
+            case "1":
+
+                if (DiasDeEstadia <= 5)
+                {
+                    costopordia = 100f + costodedesayuno * DiasDeEstadia;
+                }
+                else
+
+               if (DiasDeEstadia >= 6 - 10)
+                {
+                    costopordia = 85f + costodedesayuno * DiasDeEstadia;
+                }
+                
+                
+                if (DiasDeEstadia >= 11)
+                {
+                    costopordia = 70f + costodedesayuno * DiasDeEstadia;
+                }
+
+                Console.WriteLine("El costo complewto sale : " + costopordia);
+
+                break;
+            case "2":
+                Console.WriteLine("Ha seleccionado categoria de 5 estrellas:");
+
+                if (DiasDeEstadia <= 5)
+                {
+                    costopordia = 300f + costodedesayuno * DiasDeEstadia;
+                }else
+              
+                if (DiasDeEstadia >= 6 - 10)
+                {
+                    costopordia = 270f + costodedesayuno * DiasDeEstadia;
+                }
+               
+                if (DiasDeEstadia >= 11)
+                {
+                    costopordia = 250f + costodedesayuno * DiasDeEstadia;
+                }
+                Console.WriteLine("El costo complewto sale : " + costopordia);
+                break;
+                default:
+                {
+                    Console.WriteLine("opcion no disponible");
+                }
+                break;
+                
+            
+        }
+        Console.ReadKey();
+        Console.Read();
+    }
+}
+
+ 
